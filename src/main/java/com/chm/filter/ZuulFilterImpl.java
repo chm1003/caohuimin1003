899cc2e24191c153716356832bfe483289d1ba01
chm1003 <1741368203@qq.com>
2020-11-27 14:08:58 +0800

    123

---
+++ src/main/java/com/chm/filter/ZuulFilterImpl.java
@@ -0,0 +1,48 @@
+package com.chm.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+public class ZuulFilterImpl extends ZuulFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(ZuulFilterImpl.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        logger.info("执行过滤器，请求URL：{}，访问方法：{}" ,
+                    request.getRequestURL(),request.getMethod());
+        String accessToken = request.getHeader("accessToken");
+        if (StringUtils.isEmpty(accessToken)){
+            logger.info("当前请求头没有accessToken");
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(401);
+            return null;
+        }
+        logger.info("请求正常通过过滤器");
+        return null;
+    }
+}
